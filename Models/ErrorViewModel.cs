96b56bc1d9fc3c77023607609851bfefa4891bc4
poly <aoi.itabashi@softage.co.jp>
2026-05-22 12:10:31 +0900

    作成

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace csharp_training_202605.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
